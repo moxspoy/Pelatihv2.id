8e48e82d50a1c3195d96a9d2b8c2142a3275b593
moxspoy <mnurilmanbaehaqi@gmail.com>
2018-09-10 06:30:59 +0700

    change base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/pelatih/';
+$config['base_url'] = 'http://localhost/pelatih.id/';
 
 /*
 |--------------------------------------------------------------------------
