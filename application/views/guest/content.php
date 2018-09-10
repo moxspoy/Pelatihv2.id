6f0d6becbbb315a8ed6a552222558597bb1fb97e
moxspoy <mnurilmanbaehaqi@gmail.com>
2018-09-10 08:10:53 +0700

    customize about us section

--- application/views/guest/content.php
+++ application/views/guest/content.php
@@ -434,13 +434,15 @@
 			<div class="section-header">
 				<h3 class="section-title">Our Team</h3>
 				<span class="section-divider"></span>
-				<p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
+				<p class="section-description">
+					People behind Pelatih.id
+				</p>
 			</div>
 			<div class="row wow fadeInUp">
-				<div class="col-lg-3 col-md-6">
+				<div class="col-lg-4 col-md-6">
 					<div class="member">
-						<div class="pic"><img src="<?php echo base_url('img/team/team-1.jpg')?>" alt=""></div>
-						<h4>Walter White</h4>
+						<div class="pic"><img src="https://yt3.ggpht.com/a-/AJLlDp2c--FeEvsnv5GJ6nuTh3vAJatJF1xZ6Pa9=s900-mo-c-c0xffffffff-rj-k-no" alt=""></div>
+						<h4>M Nurilman Baehaqi</h4>
 						<span>Chief Executive Officer</span>
 						<div class="social">
 							<a href=""><i class="fa fa-twitter"></i></a>
@@ -451,25 +453,25 @@
 					</div>
 				</div>
 
-				<div class="col-lg-3 col-md-6">
+				<div class="col-lg-4 col-md-6">
 					<div class="member">
-						<div class="pic"><img src="<?php echo base_url('assets/img/team/team-2.jpg')?>" alt=""></div>
-						<h4>Sarah Jhinson</h4>
+						<div class="pic"><img src="https://scontent.fcgk9-2.fna.fbcdn.net/v/t31.0-1/c228.0.960.960/p960x960/26232631_1731024086918959_4047146794839557180_o.jpg?_nc_cat=0&_nc_eui2=AeGMHa9GMYib26azemnLUJoQstOOzQVg90BMPl4SZ8g2pWINBBuELZLHu3dGyvCvEJSZahXxAUS7S9-PmoJP1ZDkOUiqZ0P542hXOMQLB-tI_dvH6zCUjxRFRIBP5IsslUI&oh=635fb03d26124ae2e16886dbde2011ea&oe=5C288D0F" alt=""></div>
+						<h4>Lintang Dhanasmoro</h4>
 						<span>Product Manager</span>
 						<div class="social">
 							<a href=""><i class="fa fa-twitter"></i></a>
-							<a href=""><i class="fa fa-facebook"></i></a>
+							<a href="https://web.facebook.com/lintang.dhanasmoro"><i class="fa fa-facebook"></i></a>
 							<a href=""><i class="fa fa-google-plus"></i></a>
 							<a href=""><i class="fa fa-linkedin"></i></a>
 						</div>
 					</div>
 				</div>
 
-				<div class="col-lg-3 col-md-6">
+				<div class="col-lg-4 col-md-6">
 					<div class="member">
-						<div class="pic"><img src="<?php echo base_url('assets/img/team/team-3.jpg')?>" alt=""></div>
-						<h4>William Anderson</h4>
-						<span>CTO</span>
+						<div class="pic"><img src="https://pbs.twimg.com/profile_images/740200338121056256/IDH70Nd6.jpg" alt=""></div>
+						<h4>Dwi Nurfitria Bella</h4>
+						<span>Chief Marketing Officer</span>
 						<div class="social">
 							<a href=""><i class="fa fa-twitter"></i></a>
 							<a href=""><i class="fa fa-facebook"></i></a>
@@ -479,19 +481,6 @@
 					</div>
 				</div>
 
-				<div class="col-lg-3 col-md-6">
-					<div class="member">
-						<div class="pic"><img src="<?php echo base_url('assets/img/team/team-4.jpg')?>" alt=""></div>
-						<h4>Amanda Jepson</h4>
-						<span>Accountant</span>
-						<div class="social">
-							<a href=""><i class="fa fa-twitter"></i></a>
-							<a href=""><i class="fa fa-facebook"></i></a>
-							<a href=""><i class="fa fa-google-plus"></i></a>
-							<a href=""><i class="fa fa-linkedin"></i></a>
-						</div>
-					</div>
-				</div>
 			</div>
 
 		</div>
