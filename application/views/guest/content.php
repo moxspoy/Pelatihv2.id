5e7956756c89b55e37d3b2e3d6b2ae05a5191eee
moxspoy <mnurilmanbaehaqi@gmail.com>
2018-09-10 11:40:22 +0700

    change descrip product feature

--- application/views/guest/content.php
+++ application/views/guest/content.php
@@ -55,23 +55,23 @@
 
 						<div class="col-lg-6 col-md-6 box wow fadeInRight">
 							<div class="icon"><i class="ion-ios-speedometer-outline"></i></div>
-							<h4 class="title"><a href="">Lorem Ipsum</a></h4>
-							<p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident clarida perendo.</p>
+							<h4 class="title"><a href="">Search</a></h4>
+							<p class="description">Search your matching trainer. Pelatih.id let you filter to make a better and preferrable trainer</p>
 						</div>
 						<div class="col-lg-6 col-md-6 box wow fadeInRight" data-wow-delay="0.1s">
 							<div class="icon"><i class="ion-ios-flask-outline"></i></div>
-							<h4 class="title"><a href="">Dolor Sitema</a></h4>
-							<p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata noble dynala mark.</p>
+							<h4 class="title"><a href="">Order</a></h4>
+							<p class="description">After get your trainer, you can order the trainer and your trainer will contact you</p>
 						</div>
 						<div class="col-lg-6 col-md-6 box wow fadeInRight data-wow-delay="0.2s">
 						<div class="icon"><i class="ion-social-buffer-outline"></i></div>
-						<h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
-						<p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur teleca starter sinode park ledo.</p>
+						<h4 class="title"><a href="">Comparation</a></h4>
+						<p class="description">We provide a description of the available trainer you search, so you can make a decision to choose the best one</p>
 					</div>
 					<div class="col-lg-6 col-md-6 box wow fadeInRight" data-wow-delay="0.3s">
 						<div class="icon"><i class="ion-ios-analytics-outline"></i></div>
-						<h4 class="title"><a href="">Magni Dolores</a></h4>
-						<p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum dinoun trade capsule.</p>
+						<h4 class="title"><a href="">Schedule</a></h4>
+						<p class="description">Manage your schedule easily with Pelatih.id</p>
 					</div>
 				</div>
 
